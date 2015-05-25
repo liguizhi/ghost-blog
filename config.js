aa003d7e8ef46b7242a75a81ffe9cbe9ec69f768
liguizhi <liguizhi_001@163.com>
2015-05-25 20:45:24 +0800

    modify config

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://blog.tryingzone.com',
+        url: 'http://www.guizhi.me',
         mail: {},
         database: {
             client: 'sqlite3',
